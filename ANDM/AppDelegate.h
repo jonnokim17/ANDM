0d5cef5a23d54be2474bd6c0b2affd9502f4e1b8
Jonathan Kim <jonnokim17@gmail.com>
2015-08-23 13:49:29 -0400

    Initial Commit

---
+++ ANDM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ANDM
+//
+//  Created by Jonathan Kim on 8/23/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ANDM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ANDM
+//
+//  Created by Jonathan Kim on 8/23/15.
+//  Copyright (c) 2015 Jonathan Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
