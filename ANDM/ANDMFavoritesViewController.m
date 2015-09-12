07bc62b8532cdc3583ddcb6bf185f8fcbd039eab
Jonathan Kim <jonnokim17@gmail.com>
2015-09-12 14:00:58 -0400

    implemented segmented control for mapview in favoritesVC

--- ANDM/ANDMFavoritesViewController.m
+++ ANDM/ANDMFavoritesViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ANDMFavoritesViewController.h"
+#import <MapKit/MapKit.h>
 #import "SWRevealViewController.h"
 #import "FeatureBaseViewController.h"
 #import "Favorite.h"
@@ -15,10 +16,12 @@
 #import "ANDMDetailViewController.h"
 #import "NSDate+TimeAgo.h"
 
-@interface ANDMFavoritesViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface ANDMFavoritesViewController () <UITableViewDataSource, UITableViewDelegate, UIToolbarDelegate, MKMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+@property (weak, nonatomic) IBOutlet UIView *secondView;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @property (strong, nonatomic) NSArray *favoritesArray;
 
@@ -42,6 +45,8 @@
         [self.sidebarButton setAction: @selector( revealToggle: )];
         [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
     }
+
+    self.tableView.contentInset = UIEdgeInsetsMake(44,0,0,0);
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -90,10 +95,10 @@
 
     cell.textLabel.text = favoritedPage.pageName;
     cell.detailTextLabel.text = [favoritedPage.date dateTimeUntilNow];
-    
-//    [favoritedPage.image getDataInBackgroundWithBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
-//        cell.imageView.image = [UIImage imageWithData:data];
-//    }];
+
+    //    [favoritedPage.image getDataInBackgroundWithBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+    //        cell.imageView.image = [UIImage imageWithData:data];
+    //    }];
 
     return cell;
 }
@@ -106,6 +111,28 @@
     [SVProgressHUD show];
 }
 
+//- (void)configureSegmentedControl
+//{
+//    NSArray *itemArray = [NSArray arrayWithObjects: @"LIST", @"MAP", nil];
+//    UISegmentedControl *control = [[UISegmentedControl alloc] initWithItems:itemArray];
+//    [control setFrame:CGRectMake(60.0, 0, 100.0, 40.0)];
+//    [control addTarget:self action:@selector(segmentedControlHasChangedValue:) forControlEvents:UIControlEventValueChanged];
+//    [control setSelectedSegmentIndex:0];
+//    [control setEnabled:YES];
+//    self.tableView.tableHeaderView = control;
+//}
+
+//- (void)segmentedControlHasChangedValue:(id)sender
+//{
+//    UISegmentedControl *segmentedControl = (UISegmentedControl *)sender;
+//
+//    if (segmentedControl.selectedSegmentIndex == 0) {
+//        //
+//    } else if (segmentedControl.selectedSegmentIndex == 1) {
+//        //
+//    }
+//}
+
 #pragma Segue
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -115,6 +142,63 @@
 
         ANDMDetailViewController *detailVC = segue.destinationViewController;
         detailVC.selectedPage = selectedFavoritedPage;
+    }
+}
+
+#pragma mark - UIToolbarDelegate
+- (UIBarPosition)positionForBar:(id<UIBarPositioning>)bar
+{
+    return UIBarPositionTopAttached;
+}
+
+#pragma mark - MKMapViewDelegate
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+{
+    CLLocationCoordinate2D center = [view.annotation coordinate];
+    MKCoordinateSpan coordinateSpan;
+    coordinateSpan.latitudeDelta = 0.01;
+    coordinateSpan.longitudeDelta = 0.01;
+    MKCoordinateRegion coordiateRegion = MKCoordinateRegionMake(center, coordinateSpan);
+    [self.mapView setRegion:coordiateRegion animated:YES];
+}
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
+
+    pin.canShowCallout = YES;
+    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+
+    return pin;
+}
+
+#pragma mark - IBActions
+- (IBAction)onSegmentedControl:(UISegmentedControl *)sender
+{
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            NSLog(@"first index");
+            self.tableView.hidden = NO;
+            self.secondView.hidden = YES;
+            break;
+
+        case 1:
+            NSLog(@"second index");
+            self.tableView.hidden = YES;
+            self.secondView.hidden = NO;
+
+            for (Page *page in self.favoritesArray) {
+                CLLocationCoordinate2D eventCoordinate = CLLocationCoordinate2DMake(page.location.latitude, page.location.longitude);
+                MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+                annotation.coordinate = eventCoordinate;
+                annotation.title = page.pageName;
+                [self.mapView addAnnotation:annotation];
+            }
+
+            break;
+
+        default:
+            break;
     }
 }
 
